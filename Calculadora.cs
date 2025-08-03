c731bdfcdf3cc40af45fb768e72a1e144136135f
Seindexgt <jchocc7@miumg.edu.gt>
2025-08-02 23:56:35 -0600

    Actualizando nuevamente

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,6 +1,1 @@
-﻿// Programa de operaciones básicas
-Console.WriteLine("Ingrese el primer número:");
-int num1 = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Ingrese el segundo número:");
-int num2 = Convert.ToInt32(Console.ReadLine());
+﻿
