d7cf5fd3403177231a81548f8ddb7921c91d27cd
Seindexgt <jchocc7@miumg.edu.gt>
2025-08-03 00:10:16 -0600

    Actualización código de selección de operaciones - Alexander Choc

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,20 +1,24 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-﻿//Programa de operaciones basicas 
+//Programa de operaciones basicas 
 Console.WriteLine ("Ingrese el primer número:");
 int num1= 
 Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Ingrese el segundo número:");
-int num2=
-Convert.ToInt32(Consolo.ReadLine())
-=======
-﻿// Programa de operaciones básicas
+int num2 =
+Convert.ToInt32(Consolo.ReadLine());
+
+// Programa de operaciones básicas
 Console.WriteLine("Ingrese el primer número:");
 int num1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Ingrese el segundo número:");
 int num2 = Convert.ToInt32(Console.ReadLine());
->>>>>>> 58120942893a1788ee89d6c1aae290a983182b23
-=======
-﻿
->>>>>>> c731bdfcdf3cc40af45fb768e72a1e144136135f
+
+// Codigo para seleccionar la operación
+Console.WriteLine("Seleccione la operación:");
+Console.WriteLine("1 - Suma");
+Console.WriteLine("2 - Resta");
+Console.WriteLine("3 - Multiplicación");
+Console.WriteLine("4 - División");
+
+//codigo para leer la opción
+string opcion = Console.ReadLine();
