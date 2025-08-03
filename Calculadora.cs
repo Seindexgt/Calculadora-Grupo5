58120942893a1788ee89d6c1aae290a983182b23
Seindexgt <jchocc7@miumg.edu.gt>
2025-08-02 23:52:59 -0600

    Actualizando codigo

--- Calculadora.cs
+++ Calculadora.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Programa de operaciones básicas
+Console.WriteLine("Ingrese el primer número:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número:");
+int num2 = Convert.ToInt32(Console.ReadLine());
