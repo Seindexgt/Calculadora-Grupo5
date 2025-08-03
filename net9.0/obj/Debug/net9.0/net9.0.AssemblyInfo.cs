c88dcb0e06c9a8b687b9220fbb763cc81d4735e0
Seindexgt <jchocc7@miumg.edu.gt>
2025-08-03 01:12:03 -0600

    restaurando

--- net9.0/obj/Debug/net9.0/net9.0.AssemblyInfo.cs
+++ net9.0/obj/Debug/net9.0/net9.0.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("net9.0")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+000d0ee5d4566c0bfcedb7e6c034933bcc943ec4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9fdb6c7f7ec829411524ad965dddaf8dba87c27")]
 [assembly: System.Reflection.AssemblyProductAttribute("net9.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("net9.0")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
